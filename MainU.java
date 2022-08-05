dbafb5adfc6bc51fe4bbf6a97a4daf3adf05527b
stmarw <110527193+stmarw@users.noreply.github.com>
2022-08-05 16:08:05 +0900

    Add files via upload

--- MainU.java
+++ MainU.java
@@ -1,13 +1,15 @@
 package day1;
 
 import java.sql.SQLException;
+import java.util.ArrayList;
 import java.util.Scanner;
 
 public class MainU {
-
+	
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		MainU MU = new MainU();
+
 		try {
 			MU.bookstatus();
 		} catch (SQLException e) {
@@ -22,27 +24,34 @@ public class MainU {
 		int bookNo;
 		int stuNo;
 		while (!isStop) {
-			System.out.println("1. 도서정보 | 2. 도서대여 | 3. 도서반납 | 4. 도서현황 | 5. 이용현황| 6. 종료");
+			System.out.println("1. 도서정보 | 2. 도서대여 | 3. 도서반납 | 4. 이용정보 | 5. 종료");
 			System.out.print("메뉴 중 하나를 선택하세요 >>> ");
 			
 			int cmd = scan.nextInt();
 			// 선택에 맞는 작업을 한다.
 			switch(cmd) {
 			case 1:
+				infobook ib = new infobook();
 				System.out.println("1. 도서현황 | 2. 도서등록 | 3. 책정보수정 | 4. 종료.");
 				System.out.print("메뉴 중 하나를 선택하세요 >>> ");
 				int cmd2 = scan.nextInt();
 				switch(cmd2) {
 				case 1:
+					ib.bookList();
+//					System.out.println("도서목록");
+//					ArrayList<BookVO> list = dao.selectBook();
+//					System.out.println(list);
 					isStop = true;
-					System.out.println("도서목록");
+					
 					break;
 				case 2:
 					isStop = true;
 					System.out.println("책 정보를 입력하세요.");
+					ib.bookAdd();
 					break;
 				case 3:
 					isStop = true;
+					ib.updateBook();
 					System.out.print("수정 할 책의 번호를 입력해주세요.");
 					break;
 				case 4:
@@ -51,8 +60,40 @@ public class MainU {
 				}
 				break;
 			case 2:
-				System.out.println("도서대여");
-				System.out.println("");
+				System.out.println("대여목록");
+				
+				System.out.println("분류번호|    도서명      |    저자    |");
+				System.out.println("7301	야쿠자의 덕질1	  야츠다테키");
+				System.out.println("7302	헤어질 결심 각본	   박찬욱");
+				System.out.println("7303	계속 가보겠습니다	   임은정");
+				System.out.println("7304	불편한 편의점2	   김호연	");
+				System.out.println("7305	   흔한남매	   백난도	");
+				System.out.println("7306	   역행자	       자청	");
+				System.out.println("7307	   삶의격	       파스칼");
+				System.out.println("7308	   튜브	       손원평	");
+				System.out.println("7309	   자유	       조너선	");
+				System.out.println("7310	  저주토끼	       정보라");
+				System.out.println("7311	  인간실격	       다자이오사무");
+				System.out.println("7312	  아노말리	       에르베르텔리에");
+				System.out.println("7313	    류	       히가시야마아키라");
+				System.out.println("7314	  비올레트	       발레리페랭");
+				System.out.println("7315	   모비딕	       허먼멘빌");
+				System.out.println("7316	   데미안	       헤르만헤세");
+				System.out.println("7317	  베이비토크	   샐리워드");
+				System.out.println("7318	   하얼빈	       김훈");
+				System.out.println("7319	   미키7	       에드워드애슈턴");
+				System.out.println("7320     유럽도시기행2	   유시민");
+				System.out.println("7321	 하쿠다사진관	   허태연");
+				System.out.println("7322	 해커스 토익	   데이비드초");
+				System.out.println("7323	  악어의눈물	   키스포크너");
+				System.out.println("7324	 출생의 비밀	   홍성식");
+				System.out.println("7325	 묘사하는마음	   김혜리");
+				System.out.println("7326	  작별인사       김영하");
+				System.out.println("7327	 미움받을용기	   기시미이치로");
+				System.out.println("7328	5가지사랑의언어	   게리채프먼");
+				System.out.println("7329	 밥상머리교육	   김정진");
+				System.out.println("7330	    몰입	       황농문");
+				
 				System.out.println("대여하실 책의 번호를 입력해주세요>>");
 				bookNo = scan.nextInt();
 				System.out.println("학생번호를 입력해주세요 >>");
@@ -73,11 +114,8 @@ public class MainU {
 				bl.returnBook(stuNo, bookNo);
 				System.out.println("반납이 완료되었습니다.");
 				break;
-				
+		
 			case 4:
-				System.out.println("도서목록");
-				break;
-			case 5:
 				System.out.println("1. 대출 도서 상위 5위에 대한 정보");
 				System.out.println("2. 대출자 상위 5위에 대한 정보 ");
 				System.out.println("3. 대출기간이 가장 짧은 도서에 대한 정보");
@@ -119,7 +157,7 @@ public class MainU {
 				}
 				break;
 				
-			case 6:
+			case 5:
 				isStop = true;
 				System.out.println("이용해주셔서 감사합니다.");
 				break;
--- infobook.java
+++ infobook.java
@@ -1,30 +1,67 @@
-package Team;
+package day1;
+
+import java.util.Scanner;
 
 public class infobook {
-	// 책 정보 수정
+	
+	//1. 도서 목록
+	public void bookList() {
+		
+	}
+	
+	// 2. 도서 등록
+	public void bookAdd() {
+		
+	}
+	
+	// 3.책 정보 수정
     public void updateBook() {
+    	Scanner sc = new Scanner(System.in);
+    	LibraryDAO dao = new LibraryDAO();
         while (true) {
             System.out.println("수정 할 책의 번호를 입력해주세요");
             String temp = sc.nextLine();
-            int cnt = 0;
-            for (int i = 0; i < list.size(); i++) {
-                if (temp.equals(list.get(i).getbNo())) {
-                    System.out.println("제목 입력: ");
-                    list.get(i).setTitle(sc.nextLine());
-                    System.out.println("지은이 입력: ");
-                    list.get(i).setAuthor(sc.nextLine());
-                    System.out.println("가격 입력: ");
-                    list.get(i).setprice(sc.nextLine());
-                    cnt++;
-                    System.out.println("도서 수정 완료");
-                    break;
-                }
-            } // end for
-            if (cnt == 0) {
-                System.out.println("해당 도서가 존재하지 않습니다. 도서 번호를 다시 입력하세요.");
+            int book_no;
+            String title;
+            String author;
+            String price;
+            boolean flag;
+            System.out.println("책번호 입력: ");
+            book_no = sc.nextInt();
+            System.out.println("제목 입력: ");
+            title = sc.nextLine();
+            System.out.println("지은이 입력: ");
+            author = sc.nextLine();
+            System.out.println("가격 입력: ");
+            price = sc.nextLine();
+            flag = dao.updateBook(book_no, title, author, price);
+            if (flag) {
+            	System.out.println("도서 수정 완료");
+            	break;
             } else {
-                break;// 메인으로
+            	System.out.println("해당 도서가 존재하지 않습니다. 도서 번호를 다시 입력하세요.");
             }
+            
+            
+//            int cnt = 0;
+//            for (int i = 0; i < list.size(); i++) {
+//                if (temp.equals(list.get(i).getbNo())) {
+//                    System.out.println("제목 입력: ");
+//                    list.get(i).setTitle(sc.nextLine());
+//                    System.out.println("지은이 입력: ");
+//                    list.get(i).setAuthor(sc.nextLine());
+//                    System.out.println("가격 입력: ");
+//                    list.get(i).setprice(sc.nextLine());
+//                    cnt++;
+//                    System.out.println("도서 수정 완료");
+//                    break;
+//                }
+//            } // end for
+//            if (cnt == 0) {
+//                System.out.println("해당 도서가 존재하지 않습니다. 도서 번호를 다시 입력하세요.");
+//            } else {
+//                break;// 메인으로
+//            }
         } // end while
     }
 
