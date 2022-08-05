07d8984cc0b177a9be3db2376b9d676b6db70451
stmarw <110527193+stmarw@users.noreply.github.com>
2022-08-05 14:28:07 +0900

    c조 MainU 클래스

--- MainU.java
+++ MainU.java
@@ -1,4 +1,4 @@
-package Team;
+package day1;
 
 import java.sql.SQLException;
 import java.util.Scanner;
@@ -18,6 +18,9 @@ public class MainU {
 	public void bookstatus() throws SQLException {
 		Scanner scan = new Scanner(System.in);
 		boolean isStop = false;
+		BusinessLogic bl = new BusinessLogic();
+		int bookNo;
+		int stuNo;
 		while (!isStop) {
 			System.out.println("1. 도서정보 | 2. 도서대여 | 3. 도서반납 | 4. 도서현황 | 5. 이용현황| 6. 종료");
 			System.out.print("메뉴 중 하나를 선택하세요 >>> ");
@@ -48,18 +51,33 @@ public class MainU {
 				}
 				break;
 			case 2:
-				System.out.println("도서대여현황.");
-				BusinessLogic bl = new BusinessLogic();
-				bl.borrowBook(null, null);
+				System.out.println("도서대여");
+				System.out.println("");
+				System.out.println("대여하실 책의 번호를 입력해주세요>>");
+				bookNo = scan.nextInt();
+				System.out.println("학생번호를 입력해주세요 >>");
+				stuNo = scan.nextInt();
+				scan.close();
+				bl.borrowBook(stuNo, bookNo);
+				System.out.println("대여가 완료되었습니다.");
 				break;
+				
 			case 3:
-				System.out.println("도서반납현황.");
+				System.out.println("도서반납.");
+				System.out.println("");
+				System.out.println("반납하실 책의 번호를 입력해주세요>>");
+				bookNo = scan.nextInt();
+				System.out.println("학생번호를 입력해주세요 >>");
+				stuNo = scan.nextInt();
+				scan.close();
+				bl.returnBook(stuNo, bookNo);
+				System.out.println("반납이 완료되었습니다.");
 				break;
+				
 			case 4:
 				System.out.println("도서목록");
 				break;
 			case 5:
-				System.out.println("메뉴 중 하나를 선택하세요 >>> ");
 				System.out.println("1. 대출 도서 상위 5위에 대한 정보");
 				System.out.println("2. 대출자 상위 5위에 대한 정보 ");
 				System.out.println("3. 대출기간이 가장 짧은 도서에 대한 정보");
@@ -67,6 +85,7 @@ public class MainU {
 				System.out.println("5. 대출을 가장 많이하는 학과");
 				System.out.println("6. 대출반납이 가장 늦은 학과");
 				System.out.println("7. 종료.");
+				System.out.println("메뉴 중 하나를 선택하세요 >>> ");
 				int cmd3 = scan.nextInt();
 				switch(cmd3) {
 				case 1:
